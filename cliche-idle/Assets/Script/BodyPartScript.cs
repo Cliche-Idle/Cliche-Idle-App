10b88a0ff74e5471753e6d765307b8bee7f2a523
unknown <codingdummy@outlook.com>
2022-07-05 19:06:11 +0300

    Character Creator (WIP)

---
+++ cliche-idle/Assets/Script/BodyPartScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartScript : MonoBehaviour
+{
+    [Serializable]
+    public struct Sprites
+    {
+        public string name;
+        public Sprite idle;
+    }
+
+    public Sprites[] sprites;
+    int index = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
